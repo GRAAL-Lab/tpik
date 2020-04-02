5a00a534fa9d03497f044679cec89a42b4433041
Francesco <francescolaneve95@gmail.com>
2020-04-02 16:32:21 +0200

    Fixed init error in Aexternal_

--- src/Task.cpp
+++ src/Task.cpp
@@ -3,38 +3,46 @@
 namespace tpik {
 
 Task::Task(const std::string ID, int taskSpace, int DoF)
-    : ID_(ID), taskSpace_(taskSpace), DoF_(DoF), isActive_(true) {
-  //    Aexternal_.setZero(taskSpace_, taskSpace_);
-  Ai_.setZero(taskSpace_, taskSpace_);
-  x_dot_.setZero(taskSpace_);
-  J_.setZero(taskSpace_, DoF_);
-  Aexternal_.setZero(taskSpace_, taskSpace_);
+    : ID_(ID)
+    , taskSpace_(taskSpace)
+    , DoF_(DoF)
+    , isActive_(true)
+{
+    //    Aexternal_.setZero(taskSpace_, taskSpace_);
+    Ai_.setZero(taskSpace_, taskSpace_);
+    x_dot_.setZero(taskSpace_);
+    J_.setZero(taskSpace_, DoF_);
+    Aexternal_.setZero(taskSpace_, taskSpace_);
+    Aexternal_ = Eigen::Vector3d::Ones(taskSpace_).asDiagonal();
 }
 
 Task::~Task() {}
 
-const Eigen::MatrixXd &Task::GetJacobian() const { return J_; }
+const Eigen::MatrixXd& Task::GetJacobian() const { return J_; }
 
-const Eigen::MatrixXd &Task::GetInternalActivationFunction() const {
-  return Ai_;
+const Eigen::MatrixXd& Task::GetInternalActivationFunction() const
+{
+    return Ai_;
 }
 
- const Eigen::MatrixXd &Task::GetExternalActivationFunction() const {
-  return Aexternal_;
+const Eigen::MatrixXd& Task::GetExternalActivationFunction() const
+{
+    return Aexternal_;
 }
 
- void Task::SetExternalActivationFunction(Eigen::VectorXd Aexternal) {
-  if (Aexternal.size() != taskSpace_) {
-    std::cout << "Ae" << Aexternal.transpose() << std::endl;
-    std::cerr << "[TASK] ID: " << ID_
-              << " Aerows wrong dimension, taskSpace = " << taskSpace_
-              << "vector of size" << Aexternal.size() << "activating all rows"
-              << std::endl;
-  } else {
-    Aexternal_ = Aexternal.asDiagonal();
-  }
+void Task::SetExternalActivationFunction(Eigen::VectorXd Aexternal)
+{
+    if (Aexternal.size() != taskSpace_) {
+        std::cout << "Ae" << Aexternal.transpose() << std::endl;
+        std::cerr << "[TASK] ID: " << ID_
+                  << " Aerows wrong dimension, taskSpace = " << taskSpace_
+                  << "vector of size" << Aexternal.size() << "activating all rows"
+                  << std::endl;
+    } else {
+        Aexternal_ = Aexternal.asDiagonal();
+    }
 }
-const Eigen::VectorXd &Task::GetReference() const { return x_dot_; }
+const Eigen::VectorXd& Task::GetReference() const { return x_dot_; }
 
 int Task::GetDoF() { return DoF_; }
 
